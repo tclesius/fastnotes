b4fe8809e14aac96e3a184bc3ab5ca70ec0c29bf
tclesius <tomclesius@gmail.com>
2023-06-05 15:23:23 +0200

    CI-21

--- static/fastnotes/src/components/Notes.vue
+++ static/fastnotes/src/components/Notes.vue
@@ -22,14 +22,18 @@ function toggleCollapsed(id) {
   <NavBar></NavBar>
 
   <h1>Meine Notizen</h1>
-  <hr>
+  <b-input-group class="mt-3 mb-3 col-md-3 mx-auto d-flex justify-content-center">
+    <b-form-input></b-form-input>
+    <b-input-group-append>
+      <b-button variant="primary">search</b-button>
+    </b-input-group-append>
+  </b-input-group>
   <div class="container">
     <div class="row">
       <div class="col-md-12" v-for="(note, id) in notes" :key="note.id">
         <b-card style="margin-bottom: 20px; box-shadow: 0 0 0 1px lightgrey; border-radius: 5px;">
           <b-card-title>{{ note.title }}</b-card-title>
           <b-card-text>{{ note.text }}</b-card-text>
-          <hr>
           <div id="edit">
             <b-collapse :visible="expandedNoteId === id" class="mt-2">
               <span>Titel</span>
@@ -39,7 +43,6 @@ function toggleCollapsed(id) {
               <div id="ButtonUpdate">
                 <b-button variant="light" @click="notesStore.update(id, { title: note.title, text: note.text })">speichern</b-button>
               </div>
-              <hr>
             </b-collapse>
             <b-button id="ButtonEdit" variant="light" @click="toggleCollapsed(id)">bearbeiten</b-button>
             <b-button variant="light" @click="notesStore.remove(id)">löschen</b-button>
