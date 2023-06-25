d7789b46d4d0294003b922af8497a2d28ef62485
tclesius <tomclesius@gmail.com>
2023-06-25 16:55:46 +0200

    CI-125 flowbite added

--- static/fastnotes/tailwind.config.js
+++ static/fastnotes/tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   darkMode: 'class',
   theme: {
@@ -18,6 +19,7 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
+
 }
 
